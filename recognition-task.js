be122f9217dce495969eed254e2c55994b90f7d6
Carl Witt <cpw@posteo.de>
2021-04-10 23:59:34 +0200

    Removed scrollers

--- recognition-task.js
+++ recognition-task.js
@@ -5,75 +5,146 @@
  */
 
 /* ------------------------------------------------------------------------
+ * Persistent State
+ * ------------------------------------------------------------------------ */
+
+function loadState(){
+	state = JSON.parse(localStorage.chordGameState);
+}
+
+function persistState(){
+	localStorage.chordGameState = JSON.stringify(state);
+	refreshStats();
+}
+
+var defaultState = {
+		// the range of the circle of fifths that is covered. This will generate triads from keys C ± maxAccidentals, 
+		// e.g., 1 => F, C, and G 
+		'maxAccidentals': 6,
+		'language': 'german',
+		'correctAnswers': 0,
+		'wrongAnswers': 0
+	};
+
+if(localStorage.chordGameState){
+	loadState();
+} else{ 
+	state = defaultState;
+	persistState();
+}
+
+/* ------------------------------------------------------------------------
  * State
  * ------------------------------------------------------------------------ */
 
-// the offset of the current key in the circle of fifths, where 0 corresponds to 6 flats
-var currentKey = 6;
+var solution = {
+	// the offset of the currently displayed key, where 0 corresponds to 6 flats
+	'key': 6,
+	// the mode of the current chord, e.g., modes[0] = 'major'
+	'mode': modes[0],
+	// the current random triad inversion, e.g., 0 for base position, 1 for first inversion
+	'inversion': 0,
+	// the notes of the currently random chord, e.g, ['Ab', 'C', 'Eb'] for Ab major
+	'chord': []
+}
 
-// the mode of the current chord, e.g., modes[0] = 'major'
-var currentMode = modes[0];
-
-// the current random triad inversion, e.g., 0 for base position, 1 for first inversion
-var currentInversion = 0;
-
-// the notes of the currently random chord, e.g, ['Ab', 'C', 'Eb'] for Ab major
-var currentChord = [];
-
-// the range of the circle of fifths that is covered. This will generate triads from keys C ± maxAccidentals, 
-// e.g., 1 => F, C, and G 
-var maxAccidentals = 6;
-
-// the scroller that the user is currently interacting with
-var draggedScroller = undefined;
+// the key the user has currently selected (is compared to currentKey in solveAttempt)
+var solveAttempt = {
+	'key': null,
+	'mode': null,
+	'inversion': null		
+}
 
 /* ------------------------------------------------------------------------
  * Logic
  * ------------------------------------------------------------------------ */
 
-function nextChord(){
-	random = randomTriad(maxAccidentals); 
-	currentKey = random['key'];
-	currentInversion = random['inversion'];
-	currentMode = random['mode'];
-	currentChord = random['chord'];
-	highlightKeys();
-	document.querySelectorAll('.scroller').forEach(resetScroller);
-}
-
-function solveAttempt(mouseEvent){
-	var answer = mouseEvent.srcElement;
-	if(answer.key == currentKey && answer.mode == currentMode && answer.inversion == currentInversion){
-		correctAnswerGiven();
-		answer.classList.add('correct');
+function solveIfComplete(){
+	// if a part of the solution is null, don't solve yet
+	for(part in solveAttempt) if(solveAttempt[part] == null) return;
+	
+	if(solveAttempt.key == solution.key &&
+	   solveAttempt.mode == solution.mode &&
+	   solveAttempt.inversion == solution.inversion){
+  		correctAnswerGiven();
 		highlightSolution();
 	} else {
 		wrongAnswerGiven();
-		answer.classList.add('wrong');
+		// TODO show hint?
 	}
+	resetAnswer();
+	return true;
 }
+
+function resetAnswer(){
+	solveAttempt.key = null;
+	solveAttempt.mode = null;
+	solveAttempt.inversion = null;
+	
+	// unselect all key options
+	document.querySelectorAll("#solution-options td").forEach(function(element, i){element.classList.remove("selected")});	
+	// unselect all inversion options
+	document.querySelectorAll("#solution-options-inversion td").forEach(function(element, i){element.classList.remove("selected")});
+}
+
+function nextChord(){
+	random = randomTriad(state.maxAccidentals); 
+	solution.key = random['key'];
+	solution.inversion = random['inversion'];
+	solution.mode = random['mode'];
+	solution.chord = random['chord'];
+	highlightKeys();
+}
+
+function proposeKeyAndMode(mouseEvent){
+	solveAttempt.key = mouseEvent.srcElement.key;
+	solveAttempt.mode = mouseEvent.srcElement.mode;
+	
+	// unselect all key options
+	document.querySelectorAll("#solution-options td").forEach(function(element, i){element.classList.remove("selected")});	
+	mouseEvent.srcElement.classList.add('selected');
+	
+	solveIfComplete();
+}
+
+function proposeInversion(mouseEvent){
+	solveAttempt.inversion = mouseEvent.srcElement.inversion;
+	
+	// unselect all inversion options
+	document.querySelectorAll("#solution-options-inversion td").forEach(function(element, i){element.classList.remove("selected")});
+	mouseEvent.srcElement.classList.add('selected');
+	
+	solveIfComplete();
+}
+
+function removeAttemptSelection(what){
+	
+	
+}
+
+
 
 function getStats(){
 	return {
-		'correct': localStorage.correctAnswers ? Number(localStorage.correctAnswers) : 0,
-		'wrong': localStorage.wrongAnswers ? Number(localStorage.wrongAnswers) : 0
+		'correct': state.correctAnswers,
+		'wrong': state.wrongAnswers
 	}	
 }
 
 function correctAnswerGiven(){
-	localStorage.correctAnswers = getStats().correct + 1;
-	refreshStats();
+	state.correctAnswers++;
+	persistState();
 }
 
 function wrongAnswerGiven(){
-	localStorage.wrongAnswers = getStats().wrong + 1;
-	refreshStats();
+	state.wrongAnswers++;
+	persistState();
 }
 
 function resetStats(){
-	localStorage.removeItem('correctAnswers');
-	localStorage.removeItem('wrongAnswers');
-	refreshStats();
+	state.correctAnswers = 0;
+	state.wrongAnswers = 0;
+	persistState();
 }
 
 /* ------------------------------------------------------------------------
@@ -83,7 +154,7 @@ function resetStats(){
 // takes an array of note names (e.g., ['E, G, C']) and highlights the according keys on the piano
 function highlightKeys(){
 	
-	var chord = inversion(currentInversion, currentChord);
+	var chord = inversion(solution.inversion, solution.chord);
 
 	// the chord's note to highlight, e.g., first, second third, maybe fourth
 	var toHighlight = 0;
@@ -103,8 +174,8 @@ function highlightKeys(){
 function highlightSolution(){
 	
 	// use this classes to show which key belongs to which note
-	var highlightClasses = inversion(currentInversion, ['triad__prime', 'triad__third', 'triad__fifth']);
-	var chord = inversion(currentInversion, currentChord);
+	var highlightClasses = inversion(solution.inversion, ['triad__prime', 'triad__third', 'triad__fifth']);
+	var chord = inversion(solution.inversion, solution.chord);
 
 	// the chord's note to highlight, e.g., first, second third, maybe fourth
 	var toHighlight = 0;
@@ -120,49 +191,6 @@ function highlightSolution(){
 	}
 }
 
-// cycle contents of a scroller by a given number of elements
-function incrementScroller(scroller, increment){
-	
-	const currentPosition = scroller.selectedElement ? scroller.selectedElement : 0;
-	const numElements = scroller.childElementCount;
-	const newPosition = (currentPosition + increment + numElements) % numElements
-	
-	scroller.selectedElement = newPosition;
-	
-	// console.log('new scroll', scroller.children[newPosition].offsetTop - 9); //(scroller.scrollHeight + scroller.scrollTop + increment * scroller.scrollHeight / 3) % scroller.scrollHeight);
-	scroller.scrollTop = scroller.children[newPosition].offsetTop - scroller.children[0].offsetTop; // + scroller.scrollTop + increment * scroller.scrollHeight / 3) % scroller.scrollHeight;
-}
-
-function resetScroller(scroller){
-	scroller.querySelectorAll('a').forEach(a => a.classList.remove('wrong', 'correct'));
-	scroller.selectedElement = 0;
-	incrementScroller(scroller, 1);
-}
-
-// interactivity for scroller elements: when moving the mouse upwards with primary mouse button down (a drag gesture), cycle the scroller content up and vice versa.
-function dragOut(mouseEvent){
-
-	var element = mouseEvent.srcElement;
-
-	// on drag, i.e. primary mouse button is down
-	if(mouseEvent.buttons == 1 || mouseEvent.type.toLowerCase() === 'touchmove'){
-
-		// ignore events triggered during an interaction with another scroller
-		if(draggedScroller != undefined && element != draggedScroller) return;
-		
-		// mark this scroller as currently interacted with (released on mouseup anywhere)
-		draggedScroller = element;
-
-		// increment or decrement 
-		var increment = mouseEvent.offsetY < 0 ? -1 : +1;
-		incrementScroller(element, increment);		
-	}
-}
-
-document.onmouseup = function(){
-	draggedScroller = undefined;
-}
-
 // show the number of correct and wrong answers given so far
 function refreshStats(){
 	const stats = getStats();
@@ -172,72 +200,3 @@ function refreshStats(){
 	document.getElementById('percentageCorrectAnswers').innerText = totalAnswers == 0 ? 0 : Math.round(100 * stats.correct / totalAnswers);
 }
 
-// wire options control elements to state variables
-document.getElementById('input_language').onchange = function(){
-	naming = NAMING[this.value]
-	highlightKeys();
-};
-
-document.getElementById('input_maxAccidentals').onchange = function(){
-	maxAccidentals = this.value;
-	document.getElementById('label_maxAccidentals').innerText = maxAccidentals;
-};
-document.getElementById('input_maxAccidentals').onchange();
-
-function scroller(entries){
-	const div = document.createElement('div');
-	div.classList.add("scroller");
-	entries.forEach(entry => div.appendChild(entry)); //div.appendChild(document.createElement('br'));});
-	div.onmouseleave = dragOut;
-	div.ontouchmove = dragOut;
-	return div;
-}
-
-function chordLink(key, mode, inversion){
-	const link = document.createElement('a');
-	const keyName = naming.baseNote(applyAccidental(key, keys[mode].baseNote[key]), mode);
-	link.innerText = inversion == 0 ? keyName : inversion;
-	link.key = key;
-	link.mode = mode;
-	link.inversion = inversion;
-	link.onclick = solveAttempt;
-	return link;
-}
-
-// list all major/minor chords as solution options in order of the circle of fifths
-for(const mode of modes){
-	const options = document.querySelectorAll(`.solution.option.${mode} td`);
-	options.forEach(function(element, i){
-		// fill the table cell with the chord's base note name 
-		element.appendChild(scroller([chordLink(i, mode, 1), chordLink(i, mode, 0), chordLink(i, mode, 2)]));	
-
-		// var inversions = [, document.createElement('a'), document.createElement('a')];
-		// inversions.forEach(function(link, inv){
-
-		// 	element.appendChild(link);
-		// });
-		//element.innerHTML = `<a href="#">${keyName}</a> <a href="#">1</a> <a href="#">2</a>`;//keyName; //`<a>${keyName}</a>`; //<span class="inv top">1</span><span class="inv bottom">2</span>
-		// element.key = i;
-		// element.mode = mode;
-		// element.inversion = 0;
-		// element.onclick = solveAttempt;
-	})
-}
-		
-// add classes the piano keys that state their note name(s)
-// get the SVG elements that represent the piano keys
-var keyElements = Array.from(document.getElementsByClassName('key'));
-// sort the keys by horizontal position, such that we get C, C#, D, D#, etc.
-keyElements.sort((r1,r2)=>r1.attributes.x.value - r2.attributes.x.value)
-
-// add css classes to black keys that use SHARP_SYMBOL and FLAT_SYMBOL
-Array.from(document.getElementsByClassName('black key')).forEach(rect => {
-	baseNote = rect.attributes.note.value;
-	sharp = baseNote + SHARP_SYMBOL;
-	flat = enharmonicEquivalent(sharp);
-	rect.classList.add(sharp, flat);
-});
-
-// generate first chord
-nextChord();
-refreshStats();
